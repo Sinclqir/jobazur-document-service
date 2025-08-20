999e5cf2b47159bbf54555a01b89235f11ea2f72
sinclqir <sinclair.balivet@etu.unice.fr>
2025-08-20 21:54:37 +0200

    back to the origin version of index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,2 @@
-// api/index.js - Version de test
-module.exports = (req, res) => {
-    try {
-      res.json({ 
-        status: 'OK', 
-        message: 'API working',
-        path: req.url,
-        method: req.method 
-      });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
+const app = require('../src/app');
+module.exports = (req, res) => app(req, res);
